1c95d1c5acb749553e8d7c2af0a7b7e39c535a40
anushmadcourse <anushmadcourse@gmail.com>
2023-03-31 17:04:11 -0400

    node first commit

---
+++ controllers/hello-controller.js
@@ -0,0 +1,11 @@
+const HelloController = (app) => {
+  app.get("/hello", (req, res) => {
+    res.send("Life is good!");
+  });
+
+  app.get("/", (req, res) => {
+    res.send("Welcome to Full Stack Web Development!");
+  });
+};
+
+export default HelloController;
